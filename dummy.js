a171ef74cc3641d9eefafe791ed302e53f620d14
r2fresh <r2fresh78@gmail.com>
2017-03-16 14:39:56 +0900

    dummy.js data 축소 및 index.html 수정;

--- dummy.js
+++ dummy.js
@@ -23,7 +23,7 @@ module.exports = {
           "publisher": "시대고시기획",
           "pubdate": "20161005",
           "isbn": "1125426152 9791125426158",
-          "description": "- 독자대상 : <b>KT</b>그룹 채용 준비생\n- 구성 : 이론+문제+해설\n- 특징 : \n① <b>KT</b>종합 인적성검사 유형분석(2016 상반기 기출 반영)\n② 실전모의고사 1-2회 수록\n③ 인성검사 및 면접 수록"
+          "description": "- 독자대상 : <b>KT</b>그룹 채용 준비생- 구성 : 이론+문제+해설- 특징 : ① <b>KT</b>종합 인적성검사 유형분석(2016 상반기 기출 반영)② 실전모의고사 1-2회 수록③ 인성검사 및 면접 수록"
         },
         {
           "title": "기쎈 <b>KT</b>그룹 종합인적성검사(계열통합) (신입사원 및 인턴사원 채용대비)",
@@ -35,7 +35,7 @@ module.exports = {
           "publisher": "서원각",
           "pubdate": "20170111",
           "isbn": "1125716835 9791125716839",
-          "description": "- 독자대상: <b>KT</b>그룹 종합인적성검사 시험 준비생\n- 구성: 문제 + 정답 및 해설\n- 특징: \n① <b>KT</b>그룹 신입사원 및 인턴사원 채용시험 대비\n② <b>KT</b> STAR AUDITION 및 퓨쳐스타 동시 대비\n③ 수록과목 : 지각력, 언어유추력, 언어추리력, 판단력, 응용수리력, 수추리력, 공간지각력, 도식추리력, 직무해결력\n④... "
+          "description": "- 독자대상: <b>KT</b>그룹 종합인적성검사 시험 준비생- 구성: 문제 + 정답 및 해설- 특징: ① <b>KT</b>그룹 신입사원 및 인턴사원 채용시험 대비② <b>KT</b> STAR AUDITION 및 퓨쳐스타 동시 대비③ 수록과목 : 지각력, 언어유추력, 언어추리력, 판단력, 응용수리력, 수추리력, 공간지각력, 도식추리력, 직무해결력④... "
         },
         {
           "title": "<b>KT</b>그룹 채용 종합인적성검사 면접 (2015,<b>KT</b> fit test)",
@@ -47,7 +47,7 @@ module.exports = {
           "publisher": "한국고시회",
           "pubdate": "20150420",
           "isbn": "1170021832 9791170021834",
-          "description": "- 독자대상 : <b>KT</b>그룹 채용 종합인적성검사 및 면접 준비생\n- 구성 : 문제+이론\n- 특징 :\n① <b>KT</b>그룹 영역별 출제 유형 분석\n② 인성검사 모의테스트 수록\n③ <b>KT</b> FIT TEST 유형 수록\n④ <b>KT</b>그룹 면접 기출 수록"
+          "description": "- 독자대상 : <b>KT</b>그룹 채용 종합인적성검사 및 면접 준비생- 구성 : 문제+이론- 특징 :① <b>KT</b>그룹 영역별 출제 유형 분석② 인성검사 모의테스트 수록③ <b>KT</b> FIT TEST 유형 수록④ <b>KT</b>그룹 면접 기출 수록"
         },
         {
           "title": "<b>KT</b> 그룹 종합인적성검사 실전모의고사 인문계",
@@ -60,186 +60,6 @@ module.exports = {
           "pubdate": "20161005",
           "isbn": "1156461146 9791156461142",
           "description": "<b>KT</b>그룹에서 직원 채용 시에 시행하고 있는 <b>KT</b>그룹 종합인적성검사 [인문계]의 기출 유형을 철저히 분석하고 반영하여 나올 문제 유형을 모두 미리 다루어... 이 때문에 한편으로 <b>KT</b>그룹뿐만 아니라 적성검사를 치르는 다른 기업 등의 시험 대비서로도 유용할 것이다. &#x0D;&#x0D; <b>KT</b>그룹 종합인적성검사는 인력의... "
-        },
-        {
-          "title": "<b>KT</b>&G KGC 인.적성검사 길라잡이 (2015 담배인삼공사)",
-          "link": "http://book.naver.com/bookdb/book_detail.php?bid=7315566",
-          "image": "http://bookthumb.phinf.naver.net/cover/073/155/07315566.jpg?type=m1&udate=20161223",
-          "author": "기업적성연구소",
-          "price": "18000",
-          "discount": "16200",
-          "publisher": "배움",
-          "pubdate": "20130925",
-          "isbn": "8964252764 9788964252765",
-          "description": "본서는 최근 기출문제를 최대한 복원하여 <b>KT</b>&G/KGC 인·적성검사의 유형을 파악해볼 수 있도록 하였으며, 유형문제를 통해 문제에 익숙해질 수 있도록 하였다. 또한 실전 TEST를 통해 마무리를 할 수 있도록 구성하였다.\n\n일반적인 모든 직무적성검사 도서와는 달리 도서출판 배움의 도서는 단편적인... "
-        },
-        {
-          "title": "<b>KT</b> 그룹 종합인적성검사 실전모의고사 이공계",
-          "link": "http://book.naver.com/bookdb/book_detail.php?bid=10966356",
-          "image": "http://bookthumb.phinf.naver.net/cover/109/663/10966356.jpg?type=m1&udate=20161223",
-          "author": "한국고시회 편집부",
-          "price": "20000",
-          "discount": "18000",
-          "publisher": "고시넷",
-          "pubdate": "20161005",
-          "isbn": "1156461154 9791156461159",
-          "description": "<b>KT</b>그룹에서 직원 채용 시에 시행하고 있는 '<b>KT</b>그룹 종합인적성검사 [이공계]'의 기출 유형을 철저히 분석하고 반영하여 나올 문제 유형을 모두 미리 다루어... 이 때문에 한편으로 <b>KT</b>그룹뿐만 아니라 적성검사를 치르는 다른 기업 등의 시험 대비서로도 유용할 것이다.\n\n본서의 '기출근거 표시문제'들은 수험생들의... "
-        },
-        {
-          "title": "<b>KT</b>그룹 종합 인.적성검사 종합편 (2016 채용 대비)",
-          "link": "http://book.naver.com/bookdb/book_detail.php?bid=10245840",
-          "image": "http://bookthumb.phinf.naver.net/cover/102/458/10245840.jpg?type=m1&udate=20160222",
-          "author": "SD적성검사연구소",
-          "price": "20000",
-          "discount": "",
-          "publisher": "시대고시기획",
-          "pubdate": "20160310",
-          "isbn": "1125421673 9791125421672",
-          "description": "<b>KT</b>그룹 종합 인.적성검사 대비 수험서로, 2015년 하반기 최신기출문제를 복원.수록하였다. 최근... <b>KT</b>그룹 인재상과의 적합성 여부를 판별할 수 있는 인성검사를 수록하여 '나'를 표현하고 브랜드화할 수... 마지막으로 면접 실전 대책과 <b>KT</b>그룹 실제 면접을 통해 <b>KT</b>그룹 합격의 최종 관문이라 할 수 있는 면접에서... "
-        },
-        {
-          "title": "기쎈 <b>KT</b>그룹 종합인적성검사 (신입사원 및 인턴사원 채용대비)",
-          "link": "http://book.naver.com/bookdb/book_detail.php?bid=11005273",
-          "image": "http://bookthumb.phinf.naver.net/cover/110/052/11005273.jpg?type=m1&udate=20160909",
-          "author": "취업적성연구소",
-          "price": "14000",
-          "discount": "12600",
-          "publisher": "서원각",
-          "pubdate": "20160909",
-          "isbn": "1125714530 9791125714538",
-          "description": "- 독자대상 : <b>KT</b>그룹 신입사원 및 인턴사원 준비생 및 <b>KT</b> STAR AUDITION 및 퓨처스타 준비생\n- 구성 : 문제 + 해설 + 인성검사 + 면접\n- 특징 : \n① 출제예상문제와 상세한 해설 수록\n② 인성검사 및 면접기출 수록"
-        },
-        {
-          "title": "이동통신사(SKT, <b>KT</b>, LGU+) LTE 무제한 요금제 사용금지",
-          "link": "http://book.naver.com/bookdb/book_detail.php?bid=11091799",
-          "image": "http://bookthumb.phinf.naver.net/cover/110/917/11091799.jpg?type=m1&udate=20160929",
-          "author": "서울교육방송",
-          "price": "1000",
-          "discount": "",
-          "publisher": "서울문학",
-          "pubdate": "20160913",
-          "isbn": "1158829108 9791158829100",
-          "description": "해당 도서는 서울교육방송의 ‘불편한 진실 사회고발 프로그램’의 기사 모음집이다. 이동통신사 3사는 LTE 요금제와 관련해서 무제한 표현을 사용하면서도, 무제한 효과를 주지 못하였다. 이와 관련해 공정거래위원회는 경제민주화 일환으로 진행된 표시광고제를 도입해서, 이동통신사들과 상호 조율해서... "
-        },
-        {
-          "title": "<b>KT</b> 종합인적성검사 인문계 (단어유추력/직무해결력 수록)",
-          "link": "http://book.naver.com/bookdb/book_detail.php?bid=10453516",
-          "image": "http://bookthumb.phinf.naver.net/cover/104/535/10453516.jpg?type=m1&udate=20160406",
-          "author": "한국고시회",
-          "price": "22000",
-          "discount": "",
-          "publisher": "한국고시회",
-          "pubdate": "20160420",
-          "isbn": "1170023649 9791170023647",
-          "description": "'<b>KT</b> 종합인적성검사 [인문계]'에 대비하기 위한 수험서로서, <b>KT</b>그룹에서 직원 채용 시에 시행하고 있는'<b>KT</b> 종합인적성검사 [인문계]'의 기출 유형을 철저히 분석하고 반영하여 나올 문제 유형을 모두 미리 다루어 볼 수 있게 하였다. \n\n또 이 책은 최근 국내외 각 기업들의 기출문제와 관련 연구기관들이 개발한... "
-        },
-        {
-          "title": "At-Spot17. <b>KT</b>그룹 채용 종합인적성검사 (/면접, 인문계(영업마케팅))",
-          "link": "http://book.naver.com/bookdb/book_detail.php?bid=9309023",
-          "image": "http://bookthumb.phinf.naver.net/cover/093/090/09309023.jpg?type=m1&udate=20160218",
-          "author": "한국고시회",
-          "price": "20000",
-          "discount": "",
-          "publisher": "한국고시회",
-          "pubdate": "20150925",
-          "isbn": "1170022200 9791170022206",
-          "description": "- 독자대상 : <b>KT</b>그룹 취업준비생\n- 구성 : 적성검사 + 면접 + 문제\n- 특징 : \n① <b>KT</b>그룹과 전체 채용 과정을 파악할 수 있는「<b>KT</b>그룹 안내 및 종합인적성검사 안내」 수록\n② <b>KT</b>그룹의 종합인적성검사 기출 유형 분석, 유사한 유형의 문제로 재구성\n③ 인성검사 모의테스트 수록\n④ ‘<b>KT</b>그룹 면접 기출’을... "
-        },
-        {
-          "title": "<b>KT</b>그룹 종합 인.적성검사 실전편 (2016)",
-          "link": "http://book.naver.com/bookdb/book_detail.php?bid=10381845",
-          "image": "http://bookthumb.phinf.naver.net/cover/103/818/10381845.jpg?type=m1&udate=20160330",
-          "author": "SD적성검사연구소",
-          "price": "16000",
-          "discount": "14400",
-          "publisher": "시대고시기획",
-          "pubdate": "20160415",
-          "isbn": "1125422688 9791125422686",
-          "description": "<b>KT</b>그룹에 입사하고자 하는 수험생들에게 좋은 길잡이가 될 단비 같은 도서로 온라인 모의고사 1회 무료 제공과 함께 2016년 상반기 채용 대비 영역별 출제 유형 완벽 분석 및 최근 <b>KT</b>그룹 기출문제 및 출제경향을 철저히 분석해 구성한 종합 인ㆍ적성검사 실전 모의고사 2회를 수록했다."
-        },
-        {
-          "title": "<b>KT</b>&amp;G 간부급 뇌물수수 구속기소(광고 브로커 적발) (불편한 진실 사회고발 프로그램)",
-          "link": "http://book.naver.com/bookdb/book_detail.php?bid=10882289",
-          "image": "http://bookthumb.phinf.naver.net/cover/108/822/10882289.jpg?type=m1&udate=20160727",
-          "author": "서울교육방송",
-          "price": "0",
-          "discount": "",
-          "publisher": "서울문학",
-          "pubdate": "20160725",
-          "isbn": "1158826974 9791158826970",
-          "description": "해당 도서는 서울교육방송의 ‘불편한 진실’ 기사 모음집이다. 서울교육방송은 학교중심 교육정보를 보도하는 독립언론사로서, 기업경영, 교육부와 교육청과 학교 및 학생들의 살아있는 뉴스를 보도한다. 서울교육방송은 동전의 양면성, 즉 사실확인의 저널리즘을 준수하며, 사실(fact)을 기준하여... "
-        },
-        {
-          "title": "<b>KT</b>그룹 종합 인.적성검사 (2015)",
-          "link": "http://book.naver.com/bookdb/book_detail.php?bid=8945620",
-          "image": "http://bookthumb.phinf.naver.net/cover/089/456/08945620.jpg?type=m1&udate=20160218",
-          "author": "SD적성검사연구소",
-          "price": "20000",
-          "discount": "",
-          "publisher": "시대고시기획",
-          "pubdate": "20150420",
-          "isbn": "1125412755 9791125412755",
-          "description": "- 독자대상 : <b>KT</b>그룹 종합 인적성검사 시험 준비생\n- 구성 : 문제+이론\n- 특징 :\n① 최신기출문제 + 핵심이론 + 적중예상문제\n② 실전 모의고사 + 기출 면접"
-        },
-        {
-          "title": "<b>KT</b> 종합인적성검사 (수추리력 도식추리력 수록,이공계)",
-          "link": "http://book.naver.com/bookdb/book_detail.php?bid=10415953",
-          "image": "http://bookthumb.phinf.naver.net/cover/104/159/10415953.jpg?type=m1&udate=20160402",
-          "author": "한국고시회 편집부",
-          "price": "20000",
-          "discount": "",
-          "publisher": "한국고시회",
-          "pubdate": "20160420",
-          "isbn": "1170023614 9791170023616",
-          "description": "'<b>KT</b> 종합인적성검사 [이공계]'에 대비하기 위한 수험서로서, <b>KT</b>그룹에서 직원 채용 시에 시행하고 있는 '<b>KT</b> 종합인적성검사 [이공계]'의 기출 유형을 철저히 분석하고 반영하여 나올 문제 유형을 모두 미리 다루어 볼 수 있게 하였다. \n\n또 이 책은 최근 국내외 각 기업들의 기출문제와 관련 연구기관들이 개발한... "
-        },
-        {
-          "title": "<b>KT</b> 인적성 모의고사",
-          "link": "http://book.naver.com/bookdb/book_detail.php?bid=9553662",
-          "image": "http://bookthumb.phinf.naver.net/cover/095/536/09553662.jpg?type=m1&udate=20160218",
-          "author": "SD적성검사연구소",
-          "price": "16000",
-          "discount": "",
-          "publisher": "시대고시기획",
-          "pubdate": "20150925",
-          "isbn": "1125415975 9791125415978",
-          "description": "- 독자대상 : <b>KT</b>그룹 취업 준비생\n- 구성 : 문제 + 정답 및 해설\n- 특징 : 2015년 상반기 신유형 분석ㆍ반영"
-        },
-        {
-          "title": "프로야구 스카우팅 리포트 2016 - <b>kt</b> 위즈",
-          "link": "http://book.naver.com/bookdb/book_detail.php?bid=10679710",
-          "image": "http://bookthumb.phinf.naver.net/cover/106/797/10679710.jpg?type=m1&udate=20160602",
-          "author": "최훈",
-          "price": "2000",
-          "discount": "",
-          "publisher": "하빌리스",
-          "pubdate": "20160411",
-          "isbn": "1133425283 9791133425280",
-          "description": "7년간 야구팬들에게 가장 사랑받은 No.1 스카우팅 리포트\n2016년은 새로운 원년과 같은 의미를 가진 시즌이다. \n멀게는 30년 넘게 KBO리그를 지탱해온 팀들에게 변화의 바람이 불어왔다. 가깝게는 2010년대를 지배한 팀이 예기치 않게 무너지면서 군웅할거 시대의 문이 열렸다. \n안팎으로 변수가 생겼다. 우리 팀... "
-        },
-        {
-          "title": "<b>KT</b> 면접족보: 2016년 상반기 채용 면접 대비",
-          "link": "http://book.naver.com/bookdb/book_detail.php?bid=10535568",
-          "image": "http://bookthumb.phinf.naver.net/cover/105/355/10535568.jpg?type=m1&udate=20160426",
-          "author": "PMG 취업연구소",
-          "price": "2500",
-          "discount": "",
-          "publisher": "박문각",
-          "pubdate": "20160410",
-          "isbn": "1170234976 9791170234975",
-          "description": "이에 2015년 하반기 <b>KT</b>의 면접 유형을 충실히 반영한 면접족보를 e-book으로 제공한다. 이 책은 <b>KT</b>의 면접과정에 대해 파악하여 상세하게 설명하고 있다. 면접의 특징과 경향, 면접방식, 대처요령, 질문유형 분석으로 세분화하여 면접에 대해 안내한다. 또한, 작지만 유용한 면접의 디테일을 첨가하여 면접장의... "
-        },
-        {
-          "title": "At-Spot18. <b>KT</b>그룹 채용 종합인적성검사 (/면접,이공계(네트워크))",
-          "link": "http://book.naver.com/bookdb/book_detail.php?bid=9309024",
-          "image": "http://bookthumb.phinf.naver.net/cover/093/090/09309024.jpg?type=m1&udate=20160330",
-          "author": "한국고시회",
-          "price": "20000",
-          "discount": "",
-          "publisher": "한국고시회",
-          "pubdate": "20150925",
-          "isbn": "1170022219 9791170022213",
-          "description": "‘<b>KT</b>그룹 채용 종합인적성검사/면접[이공계(네트워크)]’에 대비하기 위한 수험서로서, <b>KT</b>그룹에서 직원 채용 시에 시행하고 있는 ‘<b>KT</b>그룹 채용 종합인적성검사/면접[이공계(네트워크)]’의 기출 유형을 철저히 분석하고 반영하여 나올 문제 유형을 모두 미리 다루어 볼 수 있게 하였다. \n\n또 이 책은 최근... "
         }
       ]
   }
