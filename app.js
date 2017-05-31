f605d349b87c71c766f105276d7ff94c911b71b1
r2fresh <r2fresh.kim@kt.com>
2017-05-31 16:26:48 +0900

    third test

--- app.js
+++ app.js
@@ -6,10 +6,36 @@
 */
 var express = require('express');
 var app = express();
-var dummy = require('./dummy.js')
+var dummy = require('./dummy.js');
+var restify  =require('restify');
+var moment = require('moment');
+var exphbs = require('express-handlebars');
+var acc = require('accounting')
+
+var formatter = new Intl.NumberFormat('en-US', {});
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.get('/',function(req, res){
     res.sendFile(__dirname + '/src/index.html')
+})
+
+app.get('/test', function(req, res){
+  var client = restify.createJsonClient({
+  url: 'http://localhost:8080',
+  version: '*'
+});
+client.get('/books/', function(err, req1, res1, obj) {
+//  console.log('%j', obj);
+    res.render('home',{
+      books:obj,
+      helpers:{
+        datetemp:function(d){ return moment(d,'YYYYMMDD').format('YYYY.MM.DD');},
+        moneytemp:function(m){return formatter.format(m);}
+      }
+    });
+});
 })
 
 app.use(express.static('src'));
